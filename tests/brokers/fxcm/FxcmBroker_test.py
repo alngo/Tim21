f3c418258654ae5aa9e05cdf53a3c3691a5c8a8c
alngo <alngo@student.42.fr>
2020-03-06 16:25:42 +0100

    feat(pytest): mock fxcmpy package.

--- tests/brokers/fxcm/FxcmBroker_test.py
+++ tests/brokers/fxcm/FxcmBroker_test.py
@@ -1,29 +1,48 @@
+import pandas as pd
 from tim21.brokers.fxcm.FxcmBroker import FxcmBroker
 
 
-# self.connect()
-# self.__collect_account_ids__()
-# self.default_account = self.account_ids[0]
-# msg = 'Default account set to %s, to change use set_default_account().'
-# self.logger.warning(msg % self.default_account)
-# self.__collect_orders__()
-# self.__collect_oco_orders__()
-# self.__collect_offers__()
-# self.__collect_positions__()
-# self.instruments = self.get_instruments()
-# self.subscribe_data_model('Order')
-# self.subscribe_data_model('OpenPosition')
-# self.subscribe_data_model('ClosedPosition')
+class mock_fxcmpy(object):
+    def __init__(self, access_token='', config_file='',
+                 log_file=None, log_level='', server='demo',
+                 proxy_url=None, proxy_port=None, proxy_type=None):
+        self.access_token = access_token
+        self.prices = []
+
+    def get_candles(self, instrument='', offer_id=None, period='H1', number=10,
+                    start=None, end=None, with_index=True, columns=[],
+                    stop=None):
+        pass
+
+    def subscribe_market_data(self, symbol='', add_callbacks=()):
+        pass
+
+    def get_subscribed_symbols(self):
+        return self.prices.keys()
+
+    def get_prices(self, symbol):
+        if symbol in self.prices:
+            return self.prices[symbol]
+        else:
+            return pd.DataFrame(columns=['Bid', 'Ask', 'High', 'Low'])
+
+    def unsubscribe_market_data(self, symbol=''):
+        pass
+
+    def create_market_buy_order(self, symbol, amount, account_id=None):
+        pass
+
+    def create_market_sell_order(self, symbol, amount, account_id=None):
+        pass
+
+    def get_open_positions(self, kind='dataframe'):
+        pass
 
 
 class TestFxcmBroker(object):
 
     def test_init_fxcm(self, mocker):
-        def mock__init__(self, access_token='', config_file='',
-                         log_file=None, log_level='', server='demo',
-                         proxy_url=None, proxy_port=None, proxy_type=None):
-            self.access_token = access_token
-        mocker.patch('fxcmpy.fxcmpy.__init__', mock__init__)
+        mocker.patch('fxcmpy.fxcmpy', mock_fxcmpy)
         token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         broker = FxcmBroker(account_id="test_account",
                             token="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
